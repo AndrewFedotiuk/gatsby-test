601802c129471b5b0e39f1d34c988ea9c3b825ab
Andrew <cc6.magister@gmail.com>
2020-02-28 21:01:44 +0200

    add video

--- src/components/parts/song-section-1/index.jsx
+++ src/components/parts/song-section-1/index.jsx
@@ -10,20 +10,30 @@ const SongSection1 = () => (
         <div className="row">
             <div className="image-wrapper">
                 <img className='sapho-image' src={saphoImage} alt="Sapho" />
-                <img className='star-image' src={starImage} alt="star"/>
+                <img className='star-image' src={starImage} alt="star" />
                 <p className="image-caption"><i><FormattedMessage id="songSection1ImageCaption" /></i></p>
             </div>
             <h2 className='col'><FormattedMessage id="songSection1Header" /></h2>
-            <div className="col-7">
+            <div className="col-md-7">
                 <p><FormattedMessage id="songSection1Text1" /></p>
                 <p><FormattedMessage id="songSection1Text2" /></p>
                 <p><FormattedMessage id="songSection1Text3" /></p>
                 <p><FormattedMessage id="songSection1Text4" /></p>
+
+
+            </div>
+            <div className="col-md-7">
                 <div className="poem">
                     <p><FormattedHTMLMessage id="songSection1PoemHeader" /></p>
                     <p><FormattedHTMLMessage id="songSection1Poem" /></p>
                 </div>
                 <p><FormattedMessage id="songSection1Text5" /></p>
+            </div>
+
+            <div className="col-md-5">
+                <iframe className='youtube-iframe' width="260" src="https://www.youtube.com/embed/kKMBx6x-lOI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+                <iframe className='youtube-iframe' width="260" src="https://www.youtube.com/embed/kKMBx6x-lOI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+                <iframe className='youtube-iframe' width="260" src="https://www.youtube.com/embed/kKMBx6x-lOI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
             </div>
         </div>
     </section>
